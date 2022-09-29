b85a47d7dcd275ec053f28595ca59f0752491254
ihorklimenko <ihorklymenko45@gmail.com>
2022-09-29 22:10:45 +0300

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	
+        System.out.println("111");
+        System.out.println("111");
+        System.out.println("111");
+        System.out.println("111");
     }
 }
